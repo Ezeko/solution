d15ada69c0557ddd5c58398833555e3c041aa14b
Adejobi Ezekiel <ezeko2017@gmail.com>
2020-02-21 00:01:59 +0100

    feat:
    add slave to master serve

--- useSlave.php
+++ useSlave.php
@@ -3,7 +3,7 @@ $user='database username';
 $pass ="database password";
 
 $master = new PDO('mysql:dbname=database;host=127.0.0.2', $user, $pass); //set up master
-$master->exec('update users set posts += 1'); //do INsert, Update and Delete with the master server
+$master->exec('update users set bills += 1'); //do INsert, Update and Delete with the master server
 
 $slave = new PDO('mysql:dbname=database;host=127.0.0.3', $user, $pass); //set up slave from master
-$results = $slave->query('select id from posts where user_id = 42'); //perform read operations alone with slave server
+$results = $slave->query('select id from bills where user_id = 42'); //perform read operations alone with slave server
